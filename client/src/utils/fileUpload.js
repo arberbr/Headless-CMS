c6629d6529d6f11df5296c111652a82440784a22
Arber Braja <admin@arbraja.com>
2019-05-20 12:55:17 +0200

    Code Formatting issue

--- client/src/utils/fileUpload.js
+++ client/src/utils/fileUpload.js
@@ -1,9 +1,9 @@
 const fileUpload = async files => {
 	const data = new FormData();
-	data.append("file", files[0]);
-	data.append("upload_preset", process.env.REACT_APP_CLOUDINARY_PRESET);
+	data.append('file', files[0]);
+	data.append('upload_preset', process.env.REACT_APP_CLOUDINARY_PRESET);
 	const res = await fetch(process.env.REACT_APP_CLOUDINARY_UPLOAD, {
-		method: "POST",
+		method: 'POST',
 		body: data
 	});
 	const file = await res.json();
